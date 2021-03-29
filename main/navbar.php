db0230d4c59994bf97aaed1ef3e03a5503e42d22
DaveydeHaas <daveydehaas14@gmail.com>
2021-03-30 00:54:39 +0200

    navbar letters verkleurd en vergroot

--- main/navbar.php
+++ main/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-sm navbar-light  col-12">
+<nav class="navbar navbar-expand-sm col-12">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
