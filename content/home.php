581fad570a2c3d490b5399b1d021b2c5e5929a2e
Davey <326849@student.mboutrecht.nl>
2021-03-30 10:00:14 +0200

    fix

--- content/home.php
+++ content/home.php
@@ -1,3 +1,15 @@
-Home
-Test
-Test
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+        
+        </div>
+
+        <div class="col-3">
+        
+        </div>
+        
+        <div class="col-3">
+        
+        </div>
+    </div>
+</div>
