8efec29d0eb65a5569b04840b7445afd45d79408
Davey <326849@student.mboutrecht.nl>
2021-04-05 23:28:43 +0200

    buttons op de homepage

--- content/home.php
+++ content/home.php
@@ -1,15 +1,20 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-3">
+<!-- Home stylesheet -->
+<link rel="stylesheet" href="./css/home.css">
+
+<div class="container">
+    <div class="row button-top-fix">
+        <div class="col-2"></div>
         
+        <div class="col-3">
+            <button type="button" class="btn btn-primary">Bestellen</button>
         </div>
 
         <div class="col-3">
-        
+            <button type="button" class="btn btn-primary">Reserveren</button>
         </div>
-        
+            
         <div class="col-3">
-        
+            <button type="button" class="btn btn-primary">Informatie</button>
         </div>
     </div>
 </div>
