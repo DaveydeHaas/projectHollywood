c4bb15bce9e85167dddc7911a3d55c49feeb4110
Phew <phew2002@gmail.com>
2021-04-06 09:28:44 +0200

    Info page; Cards, carousel v1

--- content/info.php
+++ content/info.php
@@ -1,16 +1,164 @@
+<link rel="stylesheet" href="./css/info.css">
+
+<!-- Container -->
 <div class="container-fluid">
-
-<!-- Section 2 -->
-<div class="row row-cols-4">
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col">Column</div>
-    <div class="col-6">Column</div>
+  <br>
+  <!-- Section 1 -->
+  <div class="row">
+    <!-- Card; Adres -->
+    <div class="col-2">
+      <div class="card card-corner" style="height: 16rem;">
+        <div class="card-body text-center">
+          <h6 class="card-title"><i class="fas fa-map-marker"></i> Adres</h6>
+          <hr class="royalblue">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Card; Adres -->
+    <!-- Card; Contact -->
+    <div class="col-2">
+      <div class="card card-corner" style="height: 16rem;">
+        <div class="card-body text-center">
+          <h6 class="card-title"><i class="fas fa-address-book"></i> Contact</h6>
+          <hr class="royalblue">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Card; Contact -->
+    <!-- Card; Opening Tijden -->
+    <div class="col-2">
+      <div class="card card-corner" style="height: 16rem;">
+        <div class="card-body text-center">
+          <h6 class="card-title"><i class="far fa-calendar-alt"></i> Opening Tijden</h6>
+          <hr class="royalblue">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Card; Opening Tijden -->
+    <!-- google maps -->
+    <div class="col-6">
+      <div class="card card-corner" style="height: 16rem;">
+        <div class="card-body text-center">
+          <h6 class="card-title"><i class="fas fa-map-marked-alt"></i> Google Maps</h6>
+          <hr class="royalblue">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.8239853640894!2d4.933875915800673!3d52.337339179779995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60bdc0973e95b%3A0xc82ad62cab0c8354!2sDuivendrechtsekade%2085%2C%201096%20AJ%20Amsterdam!5e0!3m2!1snl!2snl!4v1617635664054!5m2!1snl!2snl" class="col-12" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+      </div>
+    </div>
+    <!-- google maps -->
   </div>
+  <!-- Section 1 -->
+  <hr class="royalblue2">
+  <!-- Section 2 -->
+  <div class="row">
+    <!-- Carousel Section -->
+    <div id="carouselExampleControls" class="carousel slide col-12" data-ride="carousel">
+      <div class="carousel-inner">
+        <!-- Carousel #1 -->
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-1"></div>
+            <!-- Card #1 -->
+            <div class="card card-corner col-5" style="height: 16rem;">
+              <div class="card-body text-center">
+                <h5 class="card-title">Example #1</h5>
+                <hr class="royalblue">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <!-- Card #1 -->
+            <!-- Card #2 -->
+            <div class="card card-corner col-5" style="height: 16rem;">
+              <div class="card-body text-center">
+                <h5 class="card-title">Example #2</h5>
+                <hr class="royalblue">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <!-- Card #2 -->
+            <div class="col-1"></div>
+          </div>
+        </div>
+        <!-- Carousel #1 -->
 
-<!-- Section 1 -->
-<div class="row">
-<div class=""><p>- Carousel -</p></div>
-</div>
+        <!-- Carousel #2 -->
+        <div class="carousel-item">
+        <div class="row">
+            <div class="col-1"></div>
+            <!-- Card #1 -->
+            <div class="card card-corner col-5" style="height: 16rem;">
+              <div class="card-body text-center">
+                <h5 class="card-title">Example #3</h5>
+                <hr class="royalblue">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <!-- Card #1 -->
+            <!-- Card #2 -->
+            <div class="card card-corner col-5" style="height: 16rem;">
+              <div class="card-body text-center">
+                <h5 class="card-title">Example #4</h5>
+                <hr class="royalblue">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <!-- Card #2 -->
+            <div class="col-1"></div>
+          </div>
+        </div>
+        <!-- Carousel #2 -->
 
+        <!-- Carousel #3 -->
+        <div class="carousel-item">
+        <div class="row">
+            <div class="col-1"></div>
+            <!-- Card #1 -->
+            <div class="card card-corner col-5" style="height: 16rem;">
+              <div class="card-body text-center">
+                <h5 class="card-title">Example #5</h5>
+                <hr class="royalblue">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <!-- Card #1 -->
+            <!-- Card #2 -->
+            <div class="card card-corner col-5" style="height: 16rem;">
+              <div class="card-body text-center">
+                <h5 class="card-title">Example #6</h5>
+                <hr class="royalblue">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <!-- Card #2 -->
+            <div class="col-1"></div>
+          </div>
+        </div>
+        <!-- Carousel #3 -->
+      </div>
+      <br>
+    </div>
+    <!-- Carousel Buttons -->
+    <div class="col-5"></div>
+    <div class="col-2 card card-background-color carousel-button">
+      <div class="card-body text-center">
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="col-5"></div>
+    </div>
+    <!-- Carousel Buttons -->
+    <!-- Carousel Section -->
+  </div>
+  <br>
+  <!-- Section 2 -->
 </div>
+<!-- Container -->
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-  ob_start();
-  session_start();
-  session_gc();
+ob_start();
+session_start();
+session_gc();
 
 
 ?>
@@ -18,8 +18,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!-- Eigen stylesheet -->
   <link rel="stylesheet" href="./css/style.css">
   <!-- Tabblad Naam -->
@@ -28,24 +27,25 @@
 
 <body class="font">
   <!-- includes de navbar -->
-  <?php include("./main/navbar.php");?>
-  <!-- includes de header --> 
-  <?php include("./main/header.php");?>
+  <?php include("./main/navbar.php"); ?>
+  <!-- includes de header -->
+  <?php include("./main/header.php"); ?>
 
-      
-  <?php 
-    if (isset($_GET["content"])) {
-      include("./content/" . $_GET["content"] . ".php");
-    }else{
-      include("./content/home.php");
-    }
+
+  <?php
+  if (isset($_GET["content"])) {
+    include("./content/" . $_GET["content"] . ".php");
+  } else {
+    include("./content/home.php");
+  }
   ?>
 
   <!-- includes de footer -->
   <?php include("./main/footer.php"); ?>
 
 
-
+  <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-a11y="true"></script>
+  
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
   </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
