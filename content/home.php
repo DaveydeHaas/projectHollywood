248ff1aa62a00b3ebf01273dea60d583b5d4d1ab
DaveydeHaas <daveydehaas14@gmail.com>
2021-04-12 10:25:14 +0200

    buttons op homepage gestyled

--- content/home.php
+++ content/home.php
@@ -6,21 +6,21 @@
         <div class="col-2"></div>
         
         <div class="col-3">
-            <a href="./index.php?content=order"><button type="button" class="btn btn-primary">Bestellen</button></a>
+            <a href="./index.php?content=order"><button type="button" class="home-button">Bestellen</button></a>
         </div>
 
         <div class="col-3">
-            <a href="./index.php?content=reserve"><button type="button" class="btn btn-primary">Reserveren</button></a>
+            <a href="./index.php?content=reserve"><button type="button" class="home-button">Reserveren</button></a>
         </div>
             
         <div class="col-3">
-            <a href="./index.php?content=info"><button type="button" class="btn btn-primary">Informatie</button></a>
+            <a href="./index.php?content=info"><button type="button" class="home-button">Informatie</button></a>
         </div>
     </div>
     <div class="row button-top-fix">
         <div class="col-5"></div>
         <div class="col-3">
-            <a href="./index.php?content=menu"><button type="button" class="btn btn-primary">Menu</button></a>
+            <a href="./index.php?content=menu"><button type="button" class="home-button">Menu</button></a>
         </div>
 
     </div>
