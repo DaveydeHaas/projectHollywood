4d2a10157e376f924a3a932c721d7db4c9940906
DaveydeHaas <daveydehaas14@gmail.com>
2021-04-16 13:29:35 +0200

    een calender toegevoegd

--- content/reserve.php
+++ content/reserve.php
@@ -3,11 +3,28 @@
 
 <div class="container-fluid">
     <div class="row">
+        <div class="col-1"></div>
         <div class="col-4">
-            <div class="card backgroundcolor2">
-                Text
+            <div class="card backgroundcolor2 reserve-card">
+                <div class="">
+                    Test
+                </div>
+                <div class="">
+                    Test
+                </div>
+                <div class="">
+                <?php
+                    include('./scripts/calendar.php');
+                ?>
+                </div>
 
             </div>
+        </div>
+        <div class="col-1">
+        
+        </div>
+        <div class="col-3 backgroundcolor2 reserve-card information-card">
+        Rest
         </div>
     </div>
 
