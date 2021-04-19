9719e416f5606d75ac81e2ffb52a2974cb27f9d5
Phew <phew2002@gmail.com>
2021-04-19 23:14:17 +0200

    edit pagina

---
+++ content/edit.php
@@ -0,0 +1,169 @@
+<link rel="stylesheet" href="./css/info.css">
+
+<!-- Container -->
+<div class="container-fluid">
+    <br>
+    <!-- Section 1 -->
+    <div class="row">
+        <!-- Card; Adres -->
+        <div class="col-2">
+            <div class="card card-corner" style="height: 16rem;">
+                <div class="card-body text-center">
+                    <h6 class="card-title"><i class="fas fa-map-marker"></i> Adres</h6>
+                    <hr class="royalblue">
+                    <div class="input-group">
+                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                    </div>
+                    <br>
+                    <button type="button" class="btn btn-outline-success">Bewerk</button>
+                </div>
+            </div>
+        </div>
+        <!-- Card; Adres -->
+        <!-- Card; Contact -->
+        <div class="col-2">
+            <div class="card card-corner" style="height: 16rem;">
+                <div class="card-body text-center">
+                    <h6 class="card-title"><i class="fas fa-address-book"></i> Contact</h6>
+                    <hr class="royalblue">
+                    <div class="input-group">
+                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                    </div>
+                    <br>
+                    <button type="button" class="btn btn-outline-success">Bewerk</button>
+                </div>
+            </div>
+        </div>
+        <!-- Card; Contact -->
+        <!-- Card; Opening Tijden -->
+        <div class="col-2">
+            <div class="card card-corner" style="height: 16rem;">
+                <div class="card-body text-center">
+                    <h6 class="card-title"><i class="far fa-calendar-alt"></i> Openingstijden</h6>
+                    <hr class="royalblue">
+                    <div class="input-group">
+                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                    </div>
+                    <br>
+                    <button type="button" class="btn btn-outline-success">Bewerk</button>
+                </div>
+            </div>
+        </div>
+        <!-- Card; Openingstijden -->
+        <!-- Card; Parkeer informatie -->
+        <div class="col-6">
+            <div class="card card-corner" style="height: 16rem;">
+                <div class="card-body text-center">
+                    <h6 class="card-title"><i class="far fa-calendar-alt"></i> Parkeer Informatie</h6>
+                    <hr class="royalblue">
+                    <div class="input-group">
+                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                    </div>
+                    <br>
+                    <button type="button" class="btn btn-outline-success">Bewerk</button>
+                </div>
+            </div>
+        </div>
+        <!-- Card; Parkeer informatie -->
+    </div>
+    <!-- Section 1 -->
+    <hr class="royalblue2">
+
+    <!-- Section 3 -->
+    <div class="row">
+        <!-- Card #1 -->
+        <div class="col-1"></div>
+        <div class="card card-corner col-5" style="height: 16rem;">
+            <div class="card-body text-center">
+                <h5 class="card-title">Example #1</h5>
+                <hr class="royalblue">
+                <div class="input-group">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+                <button type="button" class="btn btn-outline-success">Bewerk</button>
+            </div>
+        </div>
+        <!-- Card #1 -->
+        <!-- Card #2 -->
+        <div class="card card-corner col-5" style="height: 16rem;">
+            <div class="card-body text-center">
+                <h5 class="card-title">Example #2</h5>
+                <hr class="royalblue">
+                <div class="input-group">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+                <br>
+                <button type="button" class="btn btn-outline-success">Bewerk</button>
+            </div>
+        </div>
+        <!-- Card #2 -->
+        <div class="col-1"></div>
+    </div>
+    <hr class="royalblue2">
+    <div class="row">
+        <!-- Card #3 -->
+        <div class="col-1"></div>
+        <div class="card card-corner col-5" style="height: 16rem;">
+            <div class="card-body text-center">
+                <h5 class="card-title">Example #3</h5>
+                <hr class="royalblue">
+                <div class="input-group">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+                <br>
+                <button type="button" class="btn btn-outline-success">Bewerk</button>
+            </div>
+        </div>
+        <!-- Card #3 -->
+
+        <!-- Card #4 -->
+        <div class="card card-corner col-5" style="height: 16rem;">
+            <div class="card-body text-center">
+                <h5 class="card-title">Example #4</h5>
+                <hr class="royalblue">
+                <div class="input-group">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+                <br>
+                <button type="button" class="btn btn-outline-success">Bewerk</button>
+            </div>
+        </div>
+        <!-- Card #4 -->
+        <div class="col-1"></div>
+    </div>
+    <hr class="royalblue2">
+    <div class="row">
+        <!-- Card #5 -->
+        <div class="col-1"></div>
+        <div class="card card-corner col-5" style="height: 16rem;">
+            <div class="card-body text-center">
+                <h5 class="card-title">Example #5</h5>
+                <hr class="royalblue">
+                <div class="input-group">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+                <br>
+                <button type="button" class="btn btn-outline-success">Bewerk</button>
+            </div>
+        </div>
+        <!-- Card #5 -->
+        <!-- Card #6 -->
+        <div class="card card-corner col-5" style="height: 16rem;">
+            <div class="card-body text-center">
+                <h5 class="card-title">Example #6</h5>
+                <hr class="royalblue">
+                <div class="input-group">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+                <br>
+                <button type="button" class="btn btn-outline-success">Bewerk</button>
+            </div>
+        </div>
+        <!-- Card #6 -->
+        <div class="col-1"></div>
+    </div>
+    <br>
+
+    <!-- Section 3 -->
+</div>
+<!-- Container -->
